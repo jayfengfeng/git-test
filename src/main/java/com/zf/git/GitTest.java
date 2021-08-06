a36f529845484bd776645f595bfcb1daf6f797f0
zf <1031575587@qq.com>
2021-08-06 11:38:41 +0800

    first commit

---
+++ src/main/java/com/zf/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zf.git;
+
+/**
+ * @author shkstart
+ * @create 2021-08-06-11:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+
+}
