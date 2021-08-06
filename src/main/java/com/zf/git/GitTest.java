97bc222239cae7c170aa9cd52ad070debfdebc04
zf <1031575587@qq.com>
2021-08-06 11:42:35 +0800

    second commit

--- src/main/java/com/zf/git/GitTest.java
+++ src/main/java/com/zf/git/GitTest.java
@@ -7,6 +7,7 @@ package com.zf.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello.git!");
+        System.out.println("hello.git2!");
     }
 
 }
